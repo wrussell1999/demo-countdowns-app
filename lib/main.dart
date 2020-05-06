e7e0d8ae1043855d0f258b00a1bd4607de568fa5
Charles de Freitas <charles@defreitas.io>
2020-05-06 22:33:29 +0100

    WIP: using name filed instead of doc id

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:quiver/async.dart';
 
 void main() => runApp(MyApp());
@@ -39,8 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
     _state = true;
     // Work out in milliseconds how long left
     var now = DateTime.now().toUtc().millisecondsSinceEpoch;
-    _secondsSinceEpoch = DateTime.now().add(Duration(minutes: _countdownTime)).millisecondsSinceEpoch;
-    var diff =  _secondsSinceEpoch - now;
+    _secondsSinceEpoch = DateTime.now()
+        .add(Duration(minutes: _countdownTime))
+        .millisecondsSinceEpoch;
+    var diff = _secondsSinceEpoch - now;
     updateCountdown();
     _doCountdown(diff);
   }
@@ -63,16 +65,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _changeCountdown(_newTime) {
     _countdownTime = _newTime;
-    _secondsSinceEpoch = DateTime.now().toUtc().add(Duration(minutes: _newTime)).millisecondsSinceEpoch;
+    _secondsSinceEpoch = DateTime.now()
+        .toUtc()
+        .add(Duration(minutes: _newTime))
+        .millisecondsSinceEpoch;
     updateCountdown();
   }
 
   void updateCountdown() {
     try {
-      database..document('1').updateData({
-          'start': _state,
-          'time': _countdownTime,
-          'epoch': _secondsSinceEpoch
+      database
+        ..where('name', isEqualTo: '1').getDocuments().then((value) {
+          value.documents.forEach((element) {
+            element.reference.updateData({
+              'start': _state,
+              'time': _countdownTime,
+              'epoch': _secondsSinceEpoch
+            });
+          });
         });
     } catch (e) {
       print(e.toString());
@@ -90,10 +100,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
     sub.onData((duration) {
       setState(() {
-
         int minutes = duration.remaining.inMinutes;
-        String seconds = (duration.remaining.inSeconds % 60).toString().padLeft(2, '0');
-        _countdownText = "$minutes:$seconds"; 
+        String seconds =
+            (duration.remaining.inSeconds % 60).toString().padLeft(2, '0');
+        _countdownText = "$minutes:$seconds";
       });
 
       sub.onDone(() {
@@ -102,14 +112,16 @@ class _MyHomePageState extends State<MyHomePage> {
         //showAlertDialog(context);
         sub.cancel();
       });
-    }); 
+    });
   }
 
   // Alert user that countdown has finished
   showAlertDialog(BuildContext context) {
     Widget okButton = FlatButton(
       child: Text("OK"),
-      onPressed: () { Navigator.of(context).pop();},
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
     );
     AlertDialog alert = AlertDialog(
       title: Text("Demo finished"),
@@ -137,108 +149,128 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             StreamBuilder(
-              stream: database.document('1').snapshots(),
-              builder: (context,snapshot) {
-                if (snapshot.hasData && !snapshot.hasError && snapshot.data.data.values != null) {
-                
+              stream: database
+                  .where('name', isEqualTo: '1')
+                  .getDocuments()
+                  .asStream(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    !snapshot.hasError &&
+                    snapshot.data.documents.length != 0) {
                   if (_state == false) {
-                    Map data = snapshot.data.data;
+                    // TODO # Bodge it ftw
+                    Map data = snapshot.data.documents.removeLast();
                     _countdownTime = data['time'];
                     _countdownText = "$_countdownTime:00";
-                  } 
-                  return Column (
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Text("$_countdownText",
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: new TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 100),
-                      ),
-                    ]
-                  );
-                }
-                else
+                  }
+                  return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "$_countdownText",
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: new TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 100),
+                        ),
+                      ]);
+                } else
                   return Text("Error: No Time");
               },
             ),
             SizedBox(height: 80),
             new Text(
-                "Set Minutes:",
-                style: TextStyle(fontSize: 15),
+              "Set Minutes:",
+              style: TextStyle(fontSize: 15),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
-              child:
-                TextField(
-                  onSubmitted: (value) {_changeCountdown(int.parse(value));},
-                  textInputAction: TextInputAction.done,
-                  autofocus: true,
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center
-                )
-              ),
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 20),
+                child: TextField(
+                    onSubmitted: (value) {
+                      _changeCountdown(int.parse(value));
+                    },
+                    textInputAction: TextInputAction.done,
+                    autofocus: true,
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center)),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new RaisedButton.icon(
-                  textColor: Colors.white,
-                  color: Colors.green,
-                  onPressed: _startCountdown,
-                  label: Text("Start"),
-                  icon: Icon(Icons.play_arrow),
-                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
-                ),
-                new RaisedButton.icon(
-                  textColor: Colors.white,
-                  color: Colors.red,
-                  onPressed: _stopCountdown,
-                  label: Text("Stop"),
-                  icon: Icon(Icons.stop),
-                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
-                ),
-              ]),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new RaisedButton.icon(
+                    textColor: Colors.white,
+                    color: Colors.green,
+                    onPressed: _startCountdown,
+                    label: Text("Start"),
+                    icon: Icon(Icons.play_arrow),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(20.0)),
+                  ),
+                  new RaisedButton.icon(
+                    textColor: Colors.white,
+                    color: Colors.red,
+                    onPressed: _stopCountdown,
+                    label: Text("Stop"),
+                    icon: Icon(Icons.stop),
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(20.0)),
+                  ),
+                ]),
             StreamBuilder(
-              stream: database.document('1').snapshots(),
+              stream: database
+                  .where('name', isEqualTo: '1')
+                  .getDocuments()
+                  .asStream(),
               builder: (context, snapshot) {
-                if (snapshot.hasData && !snapshot.hasError && snapshot.data.data.values != null) {
-                  
-                  Map data = snapshot.data.data;
+                if (snapshot.hasData &&
+                    !snapshot.hasError &&
+                    snapshot.data.documents.length != 0) {
+                  // TODO # Bodge it ftw
+                  Map data = snapshot.data.documents.removeLast();
                   _state = data['start'];
                   _countdownTime = data['time'];
                   _secondsSinceEpoch = data['epoch'];
 
                   // Check if another device has triggered the countdown
-                  if (_state == true && countdownTimer != null && countdownTimer.isRunning) {
+                  if (_state == true &&
+                      countdownTimer != null &&
+                      countdownTimer.isRunning) {
                     var now = DateTime.now().toUtc().millisecondsSinceEpoch;
                     var diff = _secondsSinceEpoch - now;
                     _doCountdown(diff);
                   }
-                  String endTime = DateTime.fromMillisecondsSinceEpoch(_secondsSinceEpoch).toString();
+                  String endTime =
+                      DateTime.fromMillisecondsSinceEpoch(_secondsSinceEpoch)
+                          .toString();
                   return Padding(
-                    padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                    child: Column (
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        RaisedButton.icon(
-                          textColor: Colors.white,
-                          color: Colors.orange,
-                          onPressed: _overrideCountdown,
-                          label: Text("Remote Override"),
-                          icon: Icon(Icons.publish),
-                          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),),
-                        Text("Firestore", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                        Text("start: $_state", style: TextStyle(fontSize: 16)),
-                        Text("time: $_countdownTime", style: TextStyle(fontSize: 16)),
-                        Text("epoch: $_secondsSinceEpoch", style: TextStyle(fontSize: 16)),
-                        Text("time stamp: $endTime", style: TextStyle(fontSize: 16))
-                    ])
-                  );
-                }
-                else
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            RaisedButton.icon(
+                              textColor: Colors.white,
+                              color: Colors.orange,
+                              onPressed: _overrideCountdown,
+                              label: Text("Remote Override"),
+                              icon: Icon(Icons.publish),
+                              shape: new RoundedRectangleBorder(
+                                  borderRadius:
+                                      new BorderRadius.circular(20.0)),
+                            ),
+                            Text("Firestore",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20)),
+                            Text("start: $_state",
+                                style: TextStyle(fontSize: 16)),
+                            Text("time: $_countdownTime",
+                                style: TextStyle(fontSize: 16)),
+                            Text("epoch: $_secondsSinceEpoch",
+                                style: TextStyle(fontSize: 16)),
+                            Text("time stamp: $endTime",
+                                style: TextStyle(fontSize: 16))
+                          ]));
+                } else
                   return Text("No data from Firestore");
-                },
+              },
             ),
           ],
         ),
@@ -246,4 +278,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
